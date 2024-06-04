db06f944d3f19564a1bae430ca89e92dd6ec37a1
Marika Lam <marikalam@Marikas-Air.lan>
2024-06-04 13:56:04 -1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({ posts: json }))
+  }
+  render() {
+    console.log(this.state.posts);
+    return (
+      <p>Hello world!</p>
+    );
+  }
+}
+export default App;
