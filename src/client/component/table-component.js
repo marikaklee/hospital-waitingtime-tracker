9bfb0d4f67b636e022eb9243af1bf99d2504e692
Marika Lam <marikalam@Marikas-Air.lan>
2024-06-04 15:58:14 -1000

    added API, retrieved JSON data and parsed onto website

--- src/client/component/table-component.js
+++ src/client/component/table-component.js
@@ -5,7 +5,7 @@ const TableComponent = ({ data, headers }) => {
   const rows = data.map(item => Object.values(item));
   console.log(rows);
   return (
-    <table>
+    <table className="table table-striped">
       <thead>
         <tr>
           {headers.map(header => <th key={header}>{header}</th>)}
@@ -13,15 +13,18 @@ const TableComponent = ({ data, headers }) => {
       </thead>
       <tbody>
         {rows.map((row, index) => {
-          return(
-          <tr key={index}>
-            <td>{row[0].Name}</td>
-            <td>{row[0].Mailing_Address}</td>
-            <td>{row[0].Licensed_Beds}</td>
-            <td>{row[0].Critical_Access_Hospital_}</td>
-            <td>{row[0].Phone_Number}</td>
-            <td>{row[0].Website}</td>
-          </tr>);
+          if (row[0].Island == "Oahu"){
+            return(
+              <tr key={index}>
+                <td>{row[0].Name}</td>
+                <td>{row[0].Mailing_Address}</td>
+                <td>{row[0].Licensed_Beds}</td>
+                <td>{row[0].Critical_Access_Hospital_}</td>
+                <td>{row[0].Phone_Number}</td>
+                <td>{row[0].Website}</td>
+              </tr>);
+          }
+          
         })}
       </tbody>
     </table>
